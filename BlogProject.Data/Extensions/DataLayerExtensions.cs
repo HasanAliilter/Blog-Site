5d7404be05c53aa1fc713d394a643c8cff4539d4
Hasan Ali İlter <hasanilter003@gmail.com>
2024-05-11 00:28:23 +0300

    Dependency Injection

---
+++ BlogProject.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BlogProject.Data.Repositories.Abstractions;
+using BlogProject.Data.Repositories.Concretes;
+
+namespace BlogProject.Data.Extensions
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); //IRepository e istek attığımda bana Repository nesnesini getir
+            return services;
+        }
+    }
+}
--- BlogProject.Web/Program.cs
+++ BlogProject.Web/Program.cs
@@ -1,8 +1,10 @@
 using BlogProject.Data.Context;
+using BlogProject.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt=> opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
