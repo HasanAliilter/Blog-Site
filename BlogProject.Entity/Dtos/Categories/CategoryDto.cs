7aab1155513be63ee7fd34fa1105b5123a2e41d5
Hasan Ali İlter <hasanilter003@gmail.com>
2024-05-19 22:28:41 +0300

    Categories Listin Page

--- BlogProject.Entity/Dtos/Categories/CategoryDto.cs
+++ BlogProject.Entity/Dtos/Categories/CategoryDto.cs
@@ -10,5 +10,8 @@ namespace BlogProject.Entity.Dtos.Categories
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public bool IsDeleted { get; set; }
     }
 }
---
+++ BlogProject.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlogProject.Entity.Dtos.Categories;
+using BlogProject.Service.Services.Abstractions;
+
+namespace BlogProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryServices categoryServices;
+
+        public CategoryController(ICategoryServices categoryServices)
+        {
+            this.categoryServices = categoryServices;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var category = await categoryServices.GetAllCategoriesNonDeleted();
+            return View(category);
+        }
+    }
+}
