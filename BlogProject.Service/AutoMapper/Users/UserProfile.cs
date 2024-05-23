e36e47242050270dce27d86855fb0a9adf92b6d0
Hasan Ali İlter <hasanilter003@gmail.com>
2024-05-23 23:04:41 +0300

    User Listing Operations

---
+++ BlogProject.Service/AutoMapper/Users/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BlogProject.Entity.Dtos.Categories;
+using BlogProject.Entity.Dtos.Users;
+using BlogProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Service.AutoMapper.Users
+{
+    public class UserProfile :Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDto, AppUser>().ReverseMap();
+        }
+    }
+}
---
+++ BlogProject.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BlogProject.Entity.Dtos.Users;
+using BlogProject.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly IMapper mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            var map = mapper.Map<List<UserDto>>(users);
+
+            foreach (var item in map)
+            {
+                var findUser = await userManager.FindByIdAsync(item.Id.ToString());
+                var role = string.Join("", await userManager.GetRolesAsync(findUser));
+
+                item.Role = role;
+            }
+            return View(map);
+        }
+    }
+}
